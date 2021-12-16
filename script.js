7601b08823a533d42a5f7ad92456d28b154022da
joaquin <jecorrea@protonmail.com>
2021-12-15 23:14:19 -0500

    Basic JS

---
+++ script.js
@@ -0,0 +1,8 @@
+//Bring in toggle buttons (querySelectorAll)
+const toggles = document.querySelectorAll(".faq-toggle");
+
+//Loop through the node list
+
+//Add a click event (addEventListener)
+
+//Toggle the active class on the parent node (.parentNode & classList.toggle())
